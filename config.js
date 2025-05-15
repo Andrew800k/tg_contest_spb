f843308f34b8b96344e1c089c5d3636bfce203f5
Andrew800k <cazanin2015@yandex.ru>
2025-05-15 19:26:06 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     SITE_KEY: "0x4AAAAAABBYFE9qmmawHgI1",  // Site Key из Cloudflare
-    END_POINT: "tgcontestspb.cazanin2015.workers.dev" // URL бэкенда
+    END_POINT: "https://tgcontestspb.cazanin2015.workers.dev/" // URL бэкенда
 };
 
 // Экспортируем объект CONFIG
